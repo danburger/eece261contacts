7610ccef5a258f08e69b8be2cfe536ee7c1ad657
robert.c.mccoll <robert.c.mccoll@38a094f8-d70a-11de-9f39-755c91dee682>
2009-12-07 23:50:40 +0000

    - Now tracks searches, supports add and remove - Now adds search results to the search (try sending it a ContactSwap:Response:Name:Rob McColl:Phone:2054220909: You will need to add whatever Name you are responding to to the Search list) - Connected adding a search to Brennan's sending function - Fixed a search saving / loading bug

--- ContactSwap/src/org/eece261/contactswap/SmsReceiver.java
+++ ContactSwap/src/org/eece261/contactswap/SmsReceiver.java
@@ -1,5 +1,11 @@
 package org.eece261.contactswap;
 
+
+import java.util.ArrayList;
+
+import org.eece261.contactswap.ContactSwap.search;
+import org.eece261.contactswap.ContactSwap;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,7 +14,9 @@ import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver
-{
+{		
+	ArrayList<search> alSearches;
+	
     @Override
     public void onReceive(Context context, Intent intent) 
     {
@@ -22,18 +30,29 @@ public class SmsReceiver extends BroadcastReceiver
             Object[] pdus = (Object[]) bundle.get("pdus");
             msgs = new SmsMessage[pdus.length];            
             for (int i=0; i<msgs.length; i++){
-                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);                
-                str += "SMS from " + msgs[i].getOriginatingAddress();                     
-                str += " :";
-                str += msgs[i].getMessageBody().toString();
-                str += "\n";  
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
                 
-                if(msgs[i].getMessageBody().toString().contains("ContactSwap:")) {
-                	//Do something with it
+                if(msgs[i].getMessageBody().toString().contains("ContactSwap:Response:") && ! msgs[i].getMessageBody().toString().contains("NotFound:")) {
+                	str += "Contact Found!";
+                	String Phone = "";
+                	String Name = "";
+                	int position = msgs[i].getMessageBody().toString().indexOf("Phone:") + 6;
+                	while(msgs[i].getMessageBody().toString().charAt(position) != ':') {
+                		Phone += msgs[i].getMessageBody().toString().charAt(position);
+                		position++;
+                	}
+                	position = msgs[i].getMessageBody().toString().indexOf("Name:") + 5;
+                	while(msgs[i].getMessageBody().toString().charAt(position) != ':') {
+                		Name += msgs[i].getMessageBody().toString().charAt(position);
+                		position++;
+                	}
+                	context.startActivity(new Intent(context, ContactSwap.class).putExtra("Name", Name).putExtra("Phone",Phone).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
                 }
             }
             //---display the new SMS message---
-            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            if(!str.equalsIgnoreCase("")) {
+            	Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            }
         }                         
     }
 }
