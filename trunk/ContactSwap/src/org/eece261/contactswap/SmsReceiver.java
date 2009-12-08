02a127df210fb9d42ccbdbb0982fd4b8c9555c76
robert.c.mccoll <robert.c.mccoll@38a094f8-d70a-11de-9f39-755c91dee682>
2009-12-08 08:22:50 +0000

    - Handles receiving Query - Searches contacts for number - Sends response

--- trunk/ContactSwap/src/org/eece261/contactswap/SmsReceiver.java
+++ trunk/ContactSwap/src/org/eece261/contactswap/SmsReceiver.java
@@ -7,11 +7,15 @@ import org.eece261.contactswap.ContactSwap.search;
 import org.eece261.contactswap.ContactSwap;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.Contacts.People;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.widget.Toast;
+import android.widget.Toast; 
 
 public class SmsReceiver extends BroadcastReceiver
 {		
@@ -47,6 +51,44 @@ public class SmsReceiver extends BroadcastReceiver
                 		position++;
                 	}
                 	context.startActivity(new Intent(context, ContactSwap.class).putExtra("Name", Name).putExtra("Phone",Phone).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                } else if (msgs[i].getMessageBody().toString().contains("ContactSwap:Query:") ) {
+                	str += "Query Received!";
+                	String Name = "";
+                	String Phone = "";
+                	int position = msgs[i].getMessageBody().toString().indexOf("Name:") + 5;
+                	while(msgs[i].getMessageBody().toString().charAt(position) != ':') {
+                		Name += msgs[i].getMessageBody().toString().charAt(position);
+                		position++;
+                	}
+                	
+                	Name.replace('^', ' ');
+                	
+                	ContentResolver cr = context.getContentResolver();
+					
+					Cursor cur =  cr.query(People.CONTENT_URI,null,null,null,null);
+					
+					if (cur.moveToFirst()) {
+				        int nameColumn = cur.getColumnIndex(People.NAME); 
+				        int phoneColumn = cur.getColumnIndex(People.NUMBER);
+				    
+				        do {
+				        	Toast.makeText(context, cur.getString(nameColumn), Toast.LENGTH_LONG).show();
+				            if(cur.getString(nameColumn).equalsIgnoreCase(Name)) {
+				            	Phone = cur.getString(phoneColumn);
+				            	Toast.makeText(context, Phone, Toast.LENGTH_LONG).show();
+				            }
+				        } while (cur.moveToNext());
+				    }
+					
+					String message = "";
+					if(!Phone.equalsIgnoreCase("")) {
+						message = "ContactSwap:Response:Name:" + Name + ":Phone:" + Phone + ":";
+					} else {
+						message = "ContactSwap:Response:Name:" + Name + ":NotFound:";
+					}
+						
+                	sendSMS(msgs[i].getOriginatingAddress(), message);
+                	
                 }
             }
             //---display the new SMS message---
@@ -54,5 +96,11 @@ public class SmsReceiver extends BroadcastReceiver
             	Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
             }
         }                         
+    }
+    
+    private void sendSMS(String phoneNumber, String message)
+    {        
+    	SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, null, null);        
     }
 }
