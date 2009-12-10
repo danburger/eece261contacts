6ce7a1979a36fffea9e627864b394460eb875a5e
robert.c.mccoll <robert.c.mccoll@38a094f8-d70a-11de-9f39-755c91dee682>
2009-12-10 03:56:35 +0000

    - Cleaned up code - Moved things into separate classes - Activity no longer flashes when receiving contact - File storage moved to SD card

---
+++ trunk/ContactSwap/src/org/eece261/contactswap/SearchHandler.java
@@ -0,0 +1,180 @@
+package org.eece261.contactswap;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.os.Environment;
+
+public class SearchHandler {
+	
+	public class Search {
+    	String name = "";
+    	ArrayList<String> responses = new ArrayList<String>();
+    }
+	
+	private ArrayList<Search> alSearches;
+	private File dir;
+	
+	public SearchHandler() {
+		dir = new File(Environment.getExternalStorageDirectory().getPath(), "ContactSwap");
+		dir.mkdirs();
+		
+		loadSearchesList();
+	}
+	
+	private void loadSearchesList() {
+    	FileInputStream fin;
+    	DataInputStream din;
+    	
+    	alSearches = new ArrayList<Search>();
+    	
+    	try {
+    		File f = new File(dir, "searchesList");
+    		f.createNewFile();
+			fin = new FileInputStream(f);
+			din = new DataInputStream(fin);
+			
+			while(din.available() > 0) {
+				Search temp = new Search();
+				temp.name = din.readUTF();
+				String current = din.readUTF();
+				while(!current.equalsIgnoreCase("")) {
+					temp.responses.add(current);
+					current = din.readUTF();
+				}
+				alSearches.add(temp);
+			}
+			
+			din.close();
+			fin.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+    }
+    
+    public void addSearchResult(String name, String phone) {
+    	for(int i = 0; i < alSearches.size(); i++) {
+    		if(alSearches.get(i).name.equalsIgnoreCase(name)) {
+    			alSearches.get(i).responses.add(phone);
+    		}
+    	}
+    	
+    	FileOutputStream fout;
+    	DataOutputStream dout;
+    	
+    	try {
+    		File f = new File(dir, "searchesList");
+            f.createNewFile();
+    		fout = new FileOutputStream(f);
+			dout = new DataOutputStream(fout);
+			
+			for(int i = 0; i < alSearches.size(); i++) {
+				dout.writeUTF(alSearches.get(i).name);
+				for(int j = 0; j < alSearches.get(i).responses.size(); j++) {
+					dout.writeUTF(alSearches.get(i).responses.get(j));
+				}
+				dout.writeUTF("");
+			}
+			
+			dout.close();
+			fout.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public ArrayList<String> getSearchNames() {
+    	loadSearchesList();
+    	ArrayList<String> alResults = new ArrayList<String>();
+    	for(int i = 0; i < alSearches.size(); i++) {
+    		alResults.add(alSearches.get(i).name);
+    	}
+    	return alResults;
+    }
+    
+    public ArrayList<String> getSearchResults(String name) {
+    	loadSearchesList();
+    	ArrayList<String> alResults = new ArrayList<String>();
+    	for(int i = 0; i < alSearches.size(); i++) {
+    		if(alSearches.get(i).name.equalsIgnoreCase(name)){
+    			return alSearches.get(i).responses;
+    		}
+    	}
+    	return alResults;
+    }
+    
+    public void addSearch(String name) {
+    	Search temp = new Search();
+    	temp.name = name;
+    	alSearches.add(temp);
+    	
+    	FileOutputStream fout;
+    	DataOutputStream dout;
+    	
+    	try {
+    		File f = new File(dir, "searchesList");
+            f.createNewFile();
+    		fout = new FileOutputStream(f);
+			dout = new DataOutputStream(fout);
+			
+			for(int i = 0; i < alSearches.size(); i++) {
+				dout.writeUTF(alSearches.get(i).name);
+				for(int j = 0; j < alSearches.get(i).responses.size(); j++) {
+					dout.writeUTF(alSearches.get(i).responses.get(j));
+				}
+				dout.writeUTF("");
+			}
+			
+			dout.close();
+			fout.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void removeSearch(String Name) {
+    	for(int i = 0; i < alSearches.size(); i++) {
+			if(alSearches.get(i).name.equalsIgnoreCase(Name)) {
+				alSearches.remove(i);
+			}
+    	}
+    	
+    	FileOutputStream fout;
+    	DataOutputStream dout;
+    	
+    	try {
+    		File f = new File(dir, "searchesList");
+            f.createNewFile();
+    		fout = new FileOutputStream(f);
+			dout = new DataOutputStream(fout);
+			
+			for(int i = 0; i < alSearches.size(); i++) {
+				dout.writeUTF(alSearches.get(i).name);
+				for(int j = 0; j < alSearches.get(i).responses.size(); j++) {
+					dout.writeUTF(alSearches.get(i).responses.get(j));
+				}
+				dout.writeUTF("");
+			}
+			
+			dout.close();
+			fout.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+}
