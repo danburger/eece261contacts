612aaa484b9dab317c3c5a81d5631d19f194d394
robert.c.mccoll <robert.c.mccoll@38a094f8-d70a-11de-9f39-755c91dee682>
2009-12-10 03:55:23 +0000

    - Cleaned up code - Moved things into separate classes - Activity no longer flashes when receiving contact - File storage moved to SD card

--- trunk/ContactSwap/src/org/eece261/contactswap/ContactSwap.java
+++ trunk/ContactSwap/src/org/eece261/contactswap/ContactSwap.java
@@ -1,31 +1,15 @@
 package org.eece261.contactswap;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.ListIterator;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Contacts;
-import android.telephony.SmsManager;
-import android.util.Log;
+import android.util.Log; 
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -55,28 +39,19 @@ public class ContactSwap extends Activity {
     TextView tvSearchName;
     
     ListView lvFriends;
-    ArrayList<String> alFriends;
     ListView lvSearches;
     ListView lvResults;
-    ArrayList<search> alSearches;
+    SearchHandler shSearches;
+    FriendHandler fhFriends;
     
-    private final ScheduledExecutorService scheduler =
-        Executors.newScheduledThreadPool(1);
- 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
     	super.onCreate(savedInstanceState);
-    	if(getIntent().hasExtra("Name") && getIntent().hasExtra("Phone")) {
-    		loadSearchesList		();
-    		addSearchResult(getIntent().getExtras().getString("Name"), getIntent().getExtras().getString("Phone"));
-            scheduler.schedule(deleteMessages, 1, TimeUnit.SECONDS);
-    		finish();
-    	}
-    	loadFriendsList();
-    	loadSearchesList();
-    	startMainMenu();    	
+    	shSearches = new SearchHandler();
+    	fhFriends = new FriendHandler();
+    	startMainMenu();
     }
     
     
@@ -130,7 +105,7 @@ public class ContactSwap extends Activity {
     	etName = (EditText) findViewById(R.id.etName);
     	
     	final ArrayAdapter<String> aaNames = new ArrayAdapter<String>(this, 
-        		android.R.layout.simple_list_item_1, getSearchNames());
+        		android.R.layout.simple_list_item_1, shSearches.getSearchNames());
         
     	lvSearches.setAdapter(aaNames);
     	
@@ -146,7 +121,7 @@ public class ContactSwap extends Activity {
         {
             public void onClick(View v) 
             {                
-                addSearch(etName.getText().toString());
+            	shSearches.addSearch(etName.getText().toString());
                 queryContactsForName(etName.getText().toString());
                 aaNames.notifyDataSetChanged();
                 openSearchResults(etName.getText().toString());
@@ -158,7 +133,7 @@ public class ContactSwap extends Activity {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				openSearchResults(getSearchNames().get(arg2));
+				openSearchResults(shSearches.getSearchNames().get(arg2));
 			}
 		});
     }
@@ -176,7 +151,7 @@ public class ContactSwap extends Activity {
     	tvSearchName.setText(name);
     	
     	final ArrayAdapter<String> aaResults = new ArrayAdapter<String>(this, 
-        		android.R.layout.simple_list_item_1, getSearchResults(name));
+        		android.R.layout.simple_list_item_1, shSearches.getSearchResults(name));
         
     	lvResults.setAdapter(aaResults);
     	
@@ -192,29 +167,27 @@ public class ContactSwap extends Activity {
         {
             public void onClick(View v) 
             {                
-                removeSearch(curname);
+            	shSearches.removeSearch(curname);
                 startSearchManager();
             }
         });
     	
     	lvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			//System.err.println("Could not listen on port: 8080.");
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
-
-				final String ThisWillOnlyBeUsedHERE = getSearchResults(curname).get(arg2);
 				
+				final String ThisWillOnlyBeUsedHERE = shSearches.getSearchResults(curname).get(arg2);	
 				new AlertDialog.Builder(ContactSwap.this).setTitle("Are you sure?")
-				.setMessage("Are you sure you want to add " + curname + " with number " + ThisWillOnlyBeUsedHERE + "?")
-				.setNeutralButton("No",
-				new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog,
-				int which) {
-				}
-				}).setPositiveButton("Yes", 
-						new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog,
-					int which) {
-						addContact(curname, ThisWillOnlyBeUsedHERE);
-					}}).show();
+					.setMessage("Are you sure you want to add " + curname + " with number " + ThisWillOnlyBeUsedHERE + "?")
+					.setNeutralButton("No",	new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {}
+					})
+					.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							addContact(curname, ThisWillOnlyBeUsedHERE);
+						}
+					})
+					.show();
 			}
 		});
     }
@@ -228,7 +201,7 @@ public class ContactSwap extends Activity {
     	etName = (EditText) findViewById(R.id.etName);
     	
     	final ArrayAdapter<String> aaFriends = new ArrayAdapter<String>(this, 
-        		android.R.layout.simple_list_item_1, alFriends);
+        		android.R.layout.simple_list_item_1, fhFriends.getFriends());
         
     	lvFriends.setAdapter(aaFriends);
     	
@@ -244,7 +217,7 @@ public class ContactSwap extends Activity {
         {
             public void onClick(View v) 
             {                
-                addFriend(etName.getText().toString());
+                fhFriends.addFriend(etName.getText().toString());
                 aaFriends.notifyDataSetChanged();
             }
         });
@@ -254,7 +227,7 @@ public class ContactSwap extends Activity {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				removeFriend(arg2);
+				fhFriends.removeFriend(arg2);
 				aaFriends.notifyDataSetChanged();
 			}
 		});
@@ -274,226 +247,13 @@ public class ContactSwap extends Activity {
                 String phoneNo = txtPhoneNo.getText().toString();
                 String message = txtMessage.getText().toString();                 
                 if (phoneNo.length()>0 && message.length()>0)                
-                    sendSMS(phoneNo, message);                
+                    ContactSwapUtils.sendSMS(phoneNo, message);                
                 else
-                    Toast.makeText(getBaseContext(), 
+                    Toast.makeText(ContactSwap.this.getApplicationContext(), 
                         "Please enter both phone number and message.", 
                         Toast.LENGTH_SHORT).show();
             }
         });
-    }
-    
-    public class search {
-    	String name = "";
-    	ArrayList<String> responses = new ArrayList<String>();
-    }
-    
-    public void loadSearchesList() {
-    	FileInputStream fin;
-    	DataInputStream din;
-    	
-    	alSearches = new ArrayList<search>();
-    	
-    	try {
-			fin = openFileInput("searchesList");
-			din = new DataInputStream(fin);
-			
-			while(din.available() > 0) {
-				search temp = new search();
-				temp.name = din.readUTF();
-				String current = din.readUTF();
-				while(!current.equalsIgnoreCase("")) {
-					temp.responses.add(current);
-					current = din.readUTF();
-				}
-				alSearches.add(temp);
-			}
-			
-			din.close();
-			fin.close();
-		} catch (FileNotFoundException e) {
-			// Assume it hasn't been created yet
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
-    public void addSearchResult(String name, String phone) {
-    	for(int i = 0; i < alSearches.size(); i++) {
-    		if(alSearches.get(i).name.equalsIgnoreCase(name)) {
-    			alSearches.get(i).responses.add(phone);
-    		}
-    	}
-    	
-    	FileOutputStream fout;
-    	DataOutputStream dout;
-    	
-    	try {
-			fout = openFileOutput("searchesList", MODE_PRIVATE);
-			dout = new DataOutputStream(fout);
-			
-			for(int i = 0; i < alSearches.size(); i++) {
-				dout.writeUTF(alSearches.get(i).name);
-				for(int j = 0; j < alSearches.get(i).responses.size(); j++) {
-					dout.writeUTF(alSearches.get(i).responses.get(j));
-				}
-				dout.writeUTF("");
-			}
-			
-			dout.close();
-			fout.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
-    private ArrayList<String> getSearchNames() {
-    	loadSearchesList();
-    	ArrayList<String> alResults = new ArrayList<String>();
-    	for(int i = 0; i < alSearches.size(); i++) {
-    		alResults.add(alSearches.get(i).name);
-    	}
-    	return alResults;
-    }
-    
-    private ArrayList<String> getSearchResults(String name) {
-    	loadSearchesList();
-    	ArrayList<String> alResults = new ArrayList<String>();
-    	for(int i = 0; i < alSearches.size(); i++) {
-    		if(alSearches.get(i).name.equalsIgnoreCase(name)){
-    			return alSearches.get(i).responses;
-    		}
-    	}
-    	return alResults;
-    }
-    
-    private void addSearch(String name) {
-    	search temp = new search();
-    	temp.name = name;
-    	alSearches.add(temp);
-    	
-    	FileOutputStream fout;
-    	DataOutputStream dout;
-    	
-    	try {
-			fout = openFileOutput("searchesList", MODE_PRIVATE);
-			dout = new DataOutputStream(fout);
-			
-			for(int i = 0; i < alSearches.size(); i++) {
-				dout.writeUTF(alSearches.get(i).name);
-				for(int j = 0; j < alSearches.get(i).responses.size(); j++) {
-					dout.writeUTF(alSearches.get(i).responses.get(j));
-				}
-				dout.writeUTF("");
-			}
-			
-			dout.close();
-			fout.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
-    private void removeSearch(String Name) {
-    	for(int i = 0; i < alSearches.size(); i++) {
-			if(alSearches.get(i).name.equalsIgnoreCase(Name)) {
-				alSearches.remove(i);
-			}
-    	}
-    	
-    	FileOutputStream fout;
-    	DataOutputStream dout;
-    	
-    	try {
-    		fout = openFileOutput("searchesList", MODE_PRIVATE);
-			dout = new DataOutputStream(fout);
-			
-			for(int i = 0; i < alSearches.size(); i++) {
-				dout.writeUTF(alSearches.get(i).name);
-				for(int j = 0; j < alSearches.get(i).responses.size(); j++) {
-					dout.writeUTF(alSearches.get(i).responses.get(j));
-				}
-				dout.writeUTF("");
-			}
-			
-			dout.close();
-			fout.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
-    private void loadFriendsList() {
-    	FileInputStream fin;
-    	DataInputStream din;
-    	
-    	alFriends = new ArrayList<String>();
-    	
-    	try {
-			fin = openFileInput("friendsList");
-			din = new DataInputStream(fin);
-			
-			while(din.available() > 0) {
-				alFriends.add(din.readUTF());
-			}
-			
-			fin.close();
-		} catch (FileNotFoundException e) {
-			// Assume it hasn't been created yet
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
-    private void addFriend(String friend) {
-    	alFriends.add(friend);
-    	
-    	FileOutputStream fout;
-    	DataOutputStream dout;
-    	
-    	try {
-			fout = openFileOutput("friendsList", MODE_PRIVATE);
-			dout = new DataOutputStream(fout);
-			
-			for(int i = 0; i < alFriends.size(); i++) {
-				dout.writeUTF(alFriends.get(i));
-			}
-			
-			fout.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
-    private void removeFriend(int index) {
-    	
-    	alFriends.remove(index);
-    	
-    	FileOutputStream fout;
-    	DataOutputStream dout;
-    	
-    	try {
-    		fout = openFileOutput("friendsList", MODE_PRIVATE);
-			dout = new DataOutputStream(fout);
-			
-			for(int i = 0; i < alFriends.size(); i++) {
-				dout.writeUTF(alFriends.get(i));
-			}
-			
-			fout.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
     }
     
     //Sends and SMS query to all "Friends"
@@ -502,87 +262,13 @@ public class ContactSwap extends Activity {
     	name.replace(' ', '^');
     	
     	String message = "ContactSwap:Query:Name:" + name + ":";
-    	ListIterator<String> alFriendsIterator = alFriends.listIterator();
+    	ListIterator<String> alFriendsIterator = fhFriends.getFriends().listIterator();
     	while(alFriendsIterator.hasNext())
     	{
-    		sendSMS(alFriendsIterator.next(),message);
+    		ContactSwapUtils.sendSMS(alFriendsIterator.next(),message);
     	}
     	return;
     }
-    
-    //---sends an SMS message to another device---
-    private void sendSMS(String phoneNumber, String message)
-    {        
-        String SENT = "SMS_SENT";
-        String DELIVERED = "SMS_DELIVERED";
- 
-        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
-            new Intent(SENT), 0);
- 
-        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
-            new Intent(DELIVERED), 0);
- 
-        //---when the SMS has been sent---
-        registerReceiver(new BroadcastReceiver(){
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(getBaseContext(), "SMS sent", 
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        Toast.makeText(getBaseContext(), "Generic failure", 
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        Toast.makeText(getBaseContext(), "No service", 
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        Toast.makeText(getBaseContext(), "Null PDU", 
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        Toast.makeText(getBaseContext(), "Radio off", 
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter(SENT));
- 
-        //---when the SMS has been delivered---
-        registerReceiver(new BroadcastReceiver(){
-
-			@Override
-			public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(getBaseContext(), "SMS delivered", 
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Toast.makeText(getBaseContext(), "SMS not delivered", 
-                                Toast.LENGTH_SHORT).show();
-                        break;                        
-                }
-				
-			}
-        }, new IntentFilter(DELIVERED));        
- 
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);        
-    }
-    
-    private final Runnable deleteMessages = new Runnable() {
-    	public void run() {
-    		Uri deleteUri = Uri.parse("content://sms");
-
-    	    getContentResolver().delete(deleteUri, "body=?", new String[] {"ContactSwap:"});
-    	}
-    };
 
     private Uri addContact(String contactName, String phoneNumber)
     {
@@ -608,7 +294,7 @@ public class ContactSwap extends Activity {
 				Log.i("CA","Failed to add new number");
 			}
     	}
-
+    	
         String addedString = "New contact added with name: " + contactName + " and number: " +phoneNumber;
        	
         Log.i("CA",addedString); 
